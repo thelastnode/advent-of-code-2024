e85472181522881bf1f415f668d08c966a472e97
Ankit Shankar <ankitshankar@gmail.com>
2024-12-02 00:22:06 -0800

    day02 refactor

--- day02/src/bin/part1.rs
+++ day02/src/bin/part1.rs
@@ -1,41 +1,5 @@
 use anyhow::Result;
-use day02::read_input;
-use itertools::Itertools;
-
-enum Direction {
-    Increasing(i32),
-    Decreasing(i32),
-    Neither,
-}
-
-fn check_report(report: &[i32]) -> bool {
-    let directions = report
-        .iter()
-        .tuple_windows()
-        .map(|(a, b)| match b - a {
-            0 => Direction::Neither,
-            diff if diff > 0 => Direction::Increasing(diff),
-            diff if diff < 0 => Direction::Decreasing(diff),
-            _ => unreachable!(),
-        })
-        .collect_vec();
-    let all_changing_in_same_direction =
-        directions
-            .iter()
-            .tuple_windows()
-            .all(|(a, b)| match (a, b) {
-                (Direction::Increasing(_), Direction::Increasing(_)) => true,
-                (Direction::Decreasing(_), Direction::Decreasing(_)) => true,
-                (Direction::Neither, _) | (_, Direction::Neither) => false,
-                _ => false,
-            });
-    let all_safe_magnitude = directions.iter().all(|direction| match direction {
-        Direction::Decreasing(diff) | Direction::Increasing(diff) => matches!(diff.abs(), 1..=3),
-        _ => false,
-    });
-
-    all_changing_in_same_direction && all_safe_magnitude
-}
+use day02::{check_report, read_input};
 
 fn process(input: Vec<Vec<i32>>) -> usize {
     input.iter().filter(|report| check_report(report)).count()
--- day02/src/lib.rs
+++ day02/src/lib.rs
@@ -1,6 +1,6 @@
-use std::io::{stdin, BufRead};
-
 use anyhow::Result;
+use itertools::Itertools;
+use std::io::{stdin, BufRead};
 
 pub fn read_input() -> Result<Vec<Vec<i32>>> {
     read_input_from(stdin().lock())
@@ -16,6 +16,42 @@ pub fn read_input_from(source: impl BufRead) -> Result<Vec<Vec<i32>>> {
                 .collect::<Result<Vec<i32>>>()
         })
         .collect()
+}
+
+#[derive(Debug)]
+pub enum Direction {
+    Increasing(i32),
+    Decreasing(i32),
+    Neither,
+}
+
+pub fn check_report(report: &[i32]) -> bool {
+    let directions = report
+        .iter()
+        .tuple_windows()
+        .map(|(a, b)| match b - a {
+            0 => Direction::Neither,
+            diff if diff > 0 => Direction::Increasing(diff),
+            diff if diff < 0 => Direction::Decreasing(diff),
+            _ => unreachable!(),
+        })
+        .collect_vec();
+    let all_changing_in_same_direction =
+        directions
+            .iter()
+            .tuple_windows()
+            .all(|(a, b)| match (a, b) {
+                (Direction::Increasing(_), Direction::Increasing(_)) => true,
+                (Direction::Decreasing(_), Direction::Decreasing(_)) => true,
+                (Direction::Neither, _) | (_, Direction::Neither) => false,
+                _ => false,
+            });
+    let all_safe_magnitude = directions.iter().all(|direction| match direction {
+        Direction::Decreasing(diff) | Direction::Increasing(diff) => matches!(diff.abs(), 1..=3),
+        _ => false,
+    });
+
+    all_changing_in_same_direction && all_safe_magnitude
 }
 
 #[cfg(test)]
